714263b731abf10858f62b257faeefc4fee55e82
Lucas Borges <lucas.borges@maino.com.br>
2025-03-05 02:03:49 -0300

    Adiciona suporte ao Action Text com validações e campos ricos para o modelo Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_rich_text :content
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :content, presence: true, length: { minimum: 10 }
+  belongs_to :user
 end
