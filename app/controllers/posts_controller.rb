861beaee0d8920da01a1abf7b401bc0c3952bbd8
Lucas Borges <lucas.borges@maino.com.br>
2025-03-08 04:52:36 -0300

    FEAT: Implementa suporte a slugs para posts, permitindo busca por slug em vez de ID

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,7 +62,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params.expect(:id))
+      @post = Post.friendly.find(params.expect(:slug))
     end
 
     def set_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
   has_rich_text :content
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true, length: { minimum: 10 }
